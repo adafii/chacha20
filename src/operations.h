6dd51c4fdbb5fc09b8c4b2638212d591b3c42084
Liia Rimpiläinen <liia@oru.fi>
2024-07-23 23:03:52 +0300

    Simplified code

--- src/operations.h
+++ src/operations.h
@@ -1,42 +1,32 @@
 #pragma once
 #include <algorithm>
 #include <array>
-#include <bit>
 #include <cstdint>
-#include <ranges>
+#include <span>
 
 namespace chacha20::operations {
 
-using chacha20_t = uint32_t;
+using chacha20_out_state_t = std::span<uint32_t, 16>;
+using chacha20_in_state_t = std::span<const uint32_t, 16>;
+using chacha20_key_t = std::span<const uint32_t, 8>;
+using chacha20_counter_t = uint32_t;
+using chacha20_nonce_t = std::array<const uint32_t, 3>;
 
-using chacha20_state_t = std::array<chacha20_t, 512 / 32>;
-using chacha20_constant_t = std::array<chacha20_t, 128 / 32>;
-using chacha20_key_t = std::array<chacha20_t, 256 / 32>;
-using chacha20_counter_t = chacha20_t;
-using chacha20_nonce_t = std::array<chacha20_t, 96 / 32>;
+constexpr auto chacha20_constant = std::array<uint32_t, 4>{0x61707865, 0x3320646e, 0x79622d32, 0x6b206574};
 
-constexpr auto get_position = [] [[nodiscard]] (auto position) consteval {
-    return [position] [[nodiscard]] (auto& state) constexpr { return std::ranges::next(state.begin(), position); };
-};
-
-constexpr auto constant_position = get_position(0);
-constexpr auto key_position = get_position(4);
+constexpr auto constant_position = 0;
+constexpr auto key_position = 4;
 constexpr auto counter_position = 12;
-constexpr auto nonce_position = get_position(13);
-constexpr auto chacha20_constant = chacha20_constant_t{0x61707865, 0x3320646e, 0x79622d32, 0x6b206574};
+constexpr auto nonce_position = 13;
 
-[[nodiscard]] constexpr chacha20_state_t create_initial_state(const chacha20_key_t& key, chacha20_counter_t counter, const chacha20_nonce_t& nonce) {
-    chacha20_state_t state;
-
-    std::ranges::copy(chacha20_constant, constant_position(state));
-    std::ranges::transform(key, key_position(state), std::byteswap<chacha20_t>);
-    state[counter_position] = counter;
-    std::ranges::transform(nonce, nonce_position(state), std::byteswap<chacha20_t>);
-
-    return state;
+constexpr void create_initial_state(chacha20_key_t key, chacha20_counter_t counter, chacha20_nonce_t nonce, chacha20_out_state_t out_state) {
+    std::ranges::copy(chacha20_constant, std::ranges::next(out_state.begin(), constant_position));
+    std::ranges::copy(key, std::ranges::next(out_state.begin(), key_position));
+    out_state[counter_position] = counter;
+    std::ranges::copy(nonce, std::ranges::next(out_state.begin(), nonce_position));
 }
 
-constexpr void quarter_round(chacha20_state_t& state, size_t a, size_t b, size_t c, size_t d) {
+constexpr void quarter_round(chacha20_out_state_t state, size_t a, size_t b, size_t c, size_t d) {
     constexpr auto round = [](auto& op1, auto& op2, auto& op3, auto rotation) constexpr {
         op1 += op2;
         op3 ^= op1;
@@ -49,8 +39,8 @@ constexpr void quarter_round(chacha20_state_t& state, size_t a, size_t b, size_t
     round(state[c], state[d], state[b], 7);
 }
 
-[[nodiscard]] constexpr chacha20_state_t chacha20_block(const chacha20_state_t& in_state) {
-    auto out_state = chacha20_state_t{in_state};
+constexpr void chacha20_block(chacha20_in_state_t in_state, chacha20_out_state_t out_state) {
+    std::ranges::copy(in_state, out_state.begin());
 
     for (auto i = 0; i < 10; ++i) {
         quarter_round(out_state, 0, 4, 8, 12);
@@ -64,8 +54,7 @@ constexpr void quarter_round(chacha20_state_t& state, size_t a, size_t b, size_t
         quarter_round(out_state, 3, 4, 9, 14);
     }
 
-    std::ranges::transform(in_state, out_state, out_state.begin(), [](auto in, auto out) constexpr { return std::byteswap<chacha20_t>(in + out); });
-    return out_state;
+    std::ranges::transform(in_state, out_state, out_state.begin(), [](auto in, auto out) constexpr { return std::byteswap<uint32_t>(in + out); });
 }
 
 }  // namespace chacha20::operations
--- tests/chacha20_test.cpp
+++ tests/chacha20_test.cpp
@@ -2,24 +2,27 @@
 #include <gtest/gtest.h>
 
 TEST(Operations, CreateInitialState) {
-    using chacha20::operations::chacha20_counter_t;
-    using chacha20::operations::chacha20_key_t;
-    using chacha20::operations::chacha20_nonce_t;
-    using chacha20::operations::chacha20_state_t;
+    constexpr auto key = std::bit_cast<std::array<const uint32_t, 8>, std::array<const uint8_t, 32>>(
+        {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+         0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
+         0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17,
+         0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f});
 
-    constexpr auto key = chacha20_key_t{0x00010203, 0x04050607, 0x08090a0b, 0x0c0d0e0f,
-                                        0x10111213, 0x14151617, 0x18191a1b, 0x1c1d1e1f};
-    constexpr auto counter = chacha20_counter_t{1};
-    constexpr auto nonce = chacha20_nonce_t{0x00000009, 0x0000004a, 0x00000000};
+    constexpr uint32_t counter = 1;
+    constexpr auto nonce = std::bit_cast<std::array<const uint32_t, 3>, std::array<const uint8_t, 12>>(
+        {0x00, 0x00, 0x00, 0x09,
+         0x00, 0x00, 0x00, 0x4a,
+         0x00, 0x00, 0x00, 0x00});
 
-    auto state = chacha20::operations::create_initial_state(key, counter, nonce);
+    auto output = std::array<uint32_t, 16>{};
+    chacha20::operations::create_initial_state(key, counter, nonce, output);
 
-    auto expected_output = chacha20_state_t{0x61707865, 0x3320646e, 0x79622d32, 0x6b206574,
-                                            0x03020100, 0x07060504, 0x0b0a0908, 0x0f0e0d0c,
-                                            0x13121110, 0x17161514, 0x1b1a1918, 0x1f1e1d1c,
-                                            0x00000001, 0x09000000, 0x4a000000, 0x00000000};
+    auto expected_output = std::array<uint32_t, 16>{0x61707865, 0x3320646e, 0x79622d32, 0x6b206574,
+                                                    0x03020100, 0x07060504, 0x0b0a0908, 0x0f0e0d0c,
+                                                    0x13121110, 0x17161514, 0x1b1a1918, 0x1f1e1d1c,
+                                                    0x00000001, 0x09000000, 0x4a000000, 0x00000000};
 
-    ASSERT_EQ(state, expected_output);
+    ASSERT_EQ(output, expected_output);
 }
 
 TEST(Operations, QuarterRound) {
@@ -31,19 +34,18 @@ TEST(Operations, QuarterRound) {
 }
 
 TEST(Operations, ChaChaBlock) {
-    using chacha20::operations::chacha20_state_t;
+    constexpr auto input = std::array<const uint32_t, 16>{0x61707865, 0x3320646e, 0x79622d32, 0x6b206574,
+                                                          0x03020100, 0x07060504, 0x0b0a0908, 0x0f0e0d0c,
+                                                          0x13121110, 0x17161514, 0x1b1a1918, 0x1f1e1d1c,
+                                                          0x00000001, 0x09000000, 0x4a000000, 0x00000000};
 
-    auto input = chacha20_state_t{0x61707865, 0x3320646e, 0x79622d32, 0x6b206574,
-                                  0x03020100, 0x07060504, 0x0b0a0908, 0x0f0e0d0c,
-                                  0x13121110, 0x17161514, 0x1b1a1918, 0x1f1e1d1c,
-                                  0x00000001, 0x09000000, 0x4a000000, 0x00000000};
+    constexpr auto expected_output = std::array<uint32_t, 16>{0x10f1e7e4, 0xd13b5915, 0x500fdd1f, 0xa32071c4,
+                                                              0xc7d1f4c7, 0x33c06803, 0x0422aa9a, 0xc3d46c4e,
+                                                              0xd2826446, 0x079faa09, 0x14c2d705, 0xd98b02a2,
+                                                              0xb5129cd1, 0xde164eb9, 0xcbd083e8, 0xa2503c4e};
 
-    auto expected_output = chacha20_state_t{0x10f1e7e4, 0xd13b5915, 0x500fdd1f, 0xa32071c4,
-                                            0xc7d1f4c7, 0x33c06803, 0x0422aa9a, 0xc3d46c4e,
-                                            0xd2826446, 0x079faa09, 0x14c2d705, 0xd98b02a2,
-                                            0xb5129cd1, 0xde164eb9, 0xcbd083e8, 0xa2503c4e};
-
-    auto output = chacha20::operations::chacha20_block(input);
+    auto output = std::array<uint32_t, 16>{};
+    chacha20::operations::chacha20_block(input, output);
 
     ASSERT_EQ(output, expected_output);
 }
